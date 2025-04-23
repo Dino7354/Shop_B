59d4efe7b7f583bb17fc8b299c0bcc3f6e3d6564
Dino7354 <strokeboy24@gmail.com>
2025-04-23 10:32:41 +0530

    Update shop route to use '/api/v1/generations' endpoint

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/shop', shopRoutes);
+app.use('/api/v1/generations', shopRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({messege:'Hello from Shop B!'});
